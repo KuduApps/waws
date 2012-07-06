7f2d503171c411781a623d13e0d8c0fd3d9d1cf9
Adam Watkin-Jones <github@acwj.co.uk>
2012-07-06 13:38:37 +0100

    updated .gitignore, removed script tags that might be causing deploy error, tidied up .cs

--- Waws.Web/Site.master.cs
+++ Waws.Web/Site.master.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class Site : System.Web.UI.MasterPage
+public partial class Site : MasterPage
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void Page_Load(Object sender, EventArgs e)
     {
-
     }
 }
